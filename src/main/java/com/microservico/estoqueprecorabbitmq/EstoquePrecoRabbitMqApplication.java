cf6c77b619a7d0c10cbc574ccc1f9c293d719934
joanagodoy <joanapereiragodoy@gmail.com>
2022-04-26 10:47:44 -0300

    Project Initialization and RabbitMq configuration

---
+++ src/main/java/com/microservico/estoqueprecorabbitmq/EstoquePrecoRabbitMqApplication.java
@@ -0,0 +1,13 @@
+package com.microservico.estoqueprecorabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoquePrecoRabbitMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstoquePrecoRabbitMqApplication.class, args);
+    }
+
+}
